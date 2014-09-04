a441f1daf81fedee3237949e9059f0cf750738ec
KamilahJ4e <kamilahjae@gmail.com>
2014-09-03 23:20:30 -0700

    First Commit

---
+++ BigNum.rb
@@ -0,0 +1,8 @@
+puts 'Good evening player 1!'
+puts 'So tell me, what is your favorite number?'
+fav_num = gets.chomp.to_i
+puts 'Wowza! that\'s one awesome number!'
+puts 'Hey, I heard that some people LOVE other numbers that are larger than their favorite.'
+puts 'Let\'s give it a try...'
+puts fav_num + 1
+puts 'I bet you will go gaga over this bigger, better number :D ' 
