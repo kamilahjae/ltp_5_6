6a8feb4764395929f310ae485a1239ebff004c03
KamilahJ4e <kamilahjae@gmail.com>
2014-09-03 23:21:27 -0700

    First commit

---
+++ FullName.rb
@@ -0,0 +1,12 @@
+puts 'Hi! What is your first name?'
+first_name = gets.chomp
+puts 'OK, and what is your middle name?'
+middle_name = gets.chomp
+puts 'And finally, what is your last name?'
+last_name = gets.chomp
+
+name = first_name + " " + middle_name + " " + last_name
+
+puts "Welcome to Seattle, " + name + "!"
+puts "I hope you grow to love it here. You can call on me whenever you need help"
+puts "Goodbye!" 
